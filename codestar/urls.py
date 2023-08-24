071f182ad4490d7519ce1a20332a6952193f4159
slbabin <>
2023-08-24 12:51:56 +0000

    Add post detail view

--- codestar/urls.py
+++ codestar/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),    
     path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
